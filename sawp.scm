7a9946375f78724a40decea867020baf16fdeb49
John Maxwell <jmax@jmaxhome.com>
2019-10-02 20:41:43 -0400

    Minimal function.
    We can send a URL to the server and incorporate it into an iframe.

--- sawp.scm
+++ sawp.scm
@@ -1,10 +1,29 @@
 (use awful)
 (use html-tags)
+(use intarweb)
+(use spiffy)
 
 (generate-sxml? #t)
 
 (define-page (main-page-path)
   (lambda ()
-    (<form>
-	 (<input> name: "url" type: "text")
-	 (<input> name: "submit" type: "submit"))))
+	(let ((request-parameters #f))
+
+	  (if (eq? 'POST (request-method (current-request)))
+		  (set! request-parameters
+				(read-urlencoded-request-data (current-request))))
+
+	  (<div>
+	   (<form> method: "POST"
+			   (<input> name: "url" type: "text")
+			   (<input> name: "submit" type: "submit"))
+
+	   (if request-parameters
+		   (<div>
+			(<hr>)
+			(<iframe> src: (cdr (assq 'url request-parameters))
+					  width: "75%"
+					  style: "margin-left: 10%"))
+		   ""))))
+  method: '(GET HEAD POST))
+
