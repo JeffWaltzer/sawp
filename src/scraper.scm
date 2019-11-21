f5a3b506fdd799d78c08f28504c198476ae7520e
John Maxwell <jmax@jmaxhome.com>
2019-11-20 21:10:40 -0500

    Started refactoring scraper.

--- src/scraper.scm
+++ src/scraper.scm
@@ -10,12 +10,25 @@
 (define (cached-time url)
   (alist-ref url time-cache string=?))
 
+
+;; ToDo: finish cleaning this up.
+(define (update-cache url page time)
+  (define (update-cache-alist alist x)
+	(alist-update url x alist string=?))
+
+  (define-syntax my-update
+	(syntax-rules ()
+	  ((_ alist value)
+	   (set! alist (update-cache-alist alist value)))))
+
+  (my-update page-cache page)
+  (my-update time-cache time))
+
 (define (scrape url)
   (or (alist-ref url page-cache string=?)
     (let ((fetched-copy
-            (with-input-from-request url #f read-string)))
-      (set! page-cache (alist-update url fetched-copy page-cache string=?))
-	  (set! time-cache (alist-update url (current-time) time-cache string=?))
+		   (with-input-from-request url #f read-string)))
+	  (update-cache url fetched-copy (current-time))
       fetched-copy)))
 
 (define (clear-page-cache)
