878a825a3df4b1cb447dda27bd29533dba279df3
John Maxwell <jmax@jmaxhome.com>
2019-11-20 20:54:20 -0500

    Spec refactoring.
    Pulled out common before/after blocks to an enclosing context.

--- spec/cache-spec.scm
+++ spec/cache-spec.scm
@@ -3,57 +3,41 @@
 (declare (uses scraper templates))
 
 
-(describe "scraping a page which is not in the cache"
+(describe "scraping a page"
   (before #:each
 	(clear-page-cache)
 	(stub! with-input-from-request (lambda (url junk reader) "")))
+
   (after #:each
+	(clear-page-cache)
 	(clear-stubs!))
 
-  (it "scrapes the url"
-	(expect ((lambda () (scrape "http://something.invalid")))
-			(call with-input-from-request
-				  (with "http://something.invalid" #f read-string)
-				  once))))
+  (describe "which is not in the cache"
 
-(describe "scraping a page which is in the cache"
-  (before #:each
-	(stub! with-input-from-request (lambda (url junk reader) "")))
-
-  (after #:each
-	(clear-stubs!))
-
-  (after #:each
-	(clear-page-cache))
-
-  (describe "foo"
-	(before #:each
-	  (scrape "http://something.invalid")
-	  (set! passed-url #f))
-
-	(it "doesn't do a HTTP request"
+	(it "scrapes the url"
 	  (expect ((lambda () (scrape "http://something.invalid")))
-			  (call with-input-from-request never)))))
+			  (call with-input-from-request
+					(with "http://something.invalid" #f read-string)
+					once))))
 
+  (describe "scraping a page which is in the cache"
+	(describe "foo"
+	  (before #:each
+		(scrape "http://something.invalid"))
 
-;; PENDING
-;; (test-group "save time of caching"
-;;   (scrape "http://something.invalid")
-;;   (test (current-time) (cached-time passed-url) )
-;; )
-(describe "save time of caching"
-  (define passed-url "http://something.invalid")
+	  (it "doesn't do a HTTP request"
+		(expect ((lambda () (scrape "http://something.invalid")))
+				(call with-input-from-request never)))))
 
-  (before #:each
-	(stub! with-input-from-request (lambda (url junk reader) ""))
-	(stub! current-time (lambda () 1))
-	(scrape "http://something.invalid"))
+  (describe "save time of caching"
+	(define passed-url "http://something.invalid")
 
-  (after #:each
-	(clear-stubs!))
+	(before #:each
+	  (stub! current-time (lambda () 1))
+	  (scrape "http://something.invalid"))
 
-  (it "saves the fetch time" 
-	(expect(cached-time passed-url)
-		   (be (current-time)))))
+	(it "saves the fetch time" 
+	  (expect(cached-time passed-url)
+			 (be (current-time))))))
 
 
