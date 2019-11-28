d32db931d9cf67d1f89ca48dbfddaf1a302f42f9
Jeff Waltzer <jeffwaltzer@gmail.com>
2019-11-27 20:53:43 -0500

    reorganized tests

--- spec/cache-spec.scm
+++ spec/cache-spec.scm
@@ -2,40 +2,35 @@
 (load "src/scraper")
 (declare (uses scraper templates))
 
-
 (describe "scraping a page"
   (before #:each
-	(clear-page-cache)
-	(stub! with-input-from-request (lambda (url junk reader) "")))
+    (clear-page-cache)
+    (stub! with-input-from-request (lambda (url junk reader) "")))
 
   (after #:each
-	(clear-page-cache)
-	(clear-stubs!))
+    (clear-page-cache)
+    (clear-stubs!))
 
   (describe "which is not in the cache"
+    (it "saves the fetch time"
+      (define passed-url "http://something.invalid")
+      (stub! current-time (lambda () 1))
+      (scrape "http://something.invalid")
+      (expect (cached-time passed-url)
+        (be (current-time))))
 
-		(it "does a HTTP request"
-	  (expect ((lambda () (scrape "http://something.invalid")))
-			  (call with-input-from-request
-					(with "http://something.invalid" #f read-string)
-					once))))
+    (it "does a HTTP request"
+      (expect ((lambda () (scrape "http://something.invalid")))
+        (call with-input-from-request
+          (with "http://something.invalid" #f read-string)
+          once))))
 
-	(describe "which is in the cache"
-	  (before #:each
-		(scrape "http://something.invalid"))
+  (describe "which is in the cache"
+    (before #:each
+      (scrape "http://something.invalid"))
 
-		(it "doesn't do a HTTP request"
-			(expect ((lambda () (scrape "http://something.invalid")))
-				(call with-input-from-request never))))
+    (it "doesn't do a HTTP request"
+      (expect ((lambda () (scrape "http://something.invalid")))
+        (call with-input-from-request never))))
 
-  (describe "when caching"
-		(define passed-url "http://something.invalid")
-
-		(before #:each
-			(stub! current-time (lambda () 1))
-			(scrape "http://something.invalid"))
-
-		(it "saves the fetch time"
-			(expect (cached-time passed-url)
-				(be (current-time))))))
-
+)
