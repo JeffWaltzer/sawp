b22a0b6363d5aac6977fd126ebfe37b9c875d82f
John Maxwell <jmax@jmaxhome.com>
2019-12-11 19:15:28 -0500

    To share.

--- spec/cache-spec.scm
+++ spec/cache-spec.scm
@@ -2,6 +2,10 @@
 (load "src/scraper")
 (declare (uses scraper templates))
 
+(define (debug . stuff)
+  (with-output-to-file "/dev/tty"
+	(lambda () (apply printf stuff))))
+
 (describe "scraping a page"
   (define passed-url "http://something.invalid")
   (before #:each
@@ -9,19 +13,25 @@
     (stub! with-input-from-request (lambda (url junk reader) "")))
 
   (after #:each
-    (clear-page-cache)
     (clear-stubs!))
 
   (describe "which is not in the cache"
     (it "saves the page body"
 	  (stub! with-input-from-request (lambda (url junk reader)
 									   "some fake body"))
+	  (debug "before scrape: time-cache: ~S~%" time-cache)
+	  (debug "before scrape: page-cache: ~S~%" page-cache)
+
 	  (scrape passed-url)
+
+	  (debug "after scrape: time-cache: ~S~%" time-cache)
+	  (debug "before scrape: page-cache: ~S~%" page-cache)
+
 	  (expect (cached-page passed-url)
 					  (be "some fake body")))
 
     (it "saves the fetch time"
-      (stub! current-time (lambda () 1))
+      (stub! current-time (lambda () (seconds->time 10000)))
       (scrape passed-url)
       (expect (cached-time passed-url)
         (be (current-time))))
@@ -32,12 +42,26 @@
           (with passed-url #f read-string)
           once))))
 
-  (describe "which is in the cache"
+  (describe "which is in the cache and not expired"
     (before #:each
+	  (clear-page-cache)
       (scrape passed-url))
 
     (it "doesn't do a HTTP request"
       (expect ((lambda () (scrape passed-url)))
         (call with-input-from-request never))))
 
-)
+  (describe "which is in the cache but expired"
+	(define cache-timeout 3600)
+
+    (before #:each
+	  (clear-page-cache)
+	  (update-cache "a-url"
+					"page body"
+					(seconds->time (- (time->seconds (current-time))
+									  (+ 1 cache-timeout)))))
+	(it "does a new HTTP request"
+	  (expect ((lambda () (scrape "a-url")))
+			  (call with-input-from-request
+					(with "a-url" #f read-string)
+					once)))))
--- src/scraper.scm
+++ src/scraper.scm
@@ -15,6 +15,9 @@
 
 ;; ToDo: finish cleaning this up.
 (define (update-cache url page time)
+  (debug "(update-cache ~S ~S ~S)~%" url page time)
+  (debug "update-cache: page-cache: ~S)~%" page-cache)
+
   (define (update-cache-alist alist x)
 	(alist-update url x alist string=?))
 
@@ -24,14 +27,39 @@
 	   (set! alist (update-cache-alist alist value)))))
 
   (my-update page-cache page)
-  (my-update time-cache time))
+  (my-update time-cache time)
+  (debug "update-cache: page-cache: ~S)~%" page-cache))
+
+(define cache-staleness-time 3600) 
+
+(define (debug . stuff)
+  (with-output-to-file "/dev/tty"
+	(lambda () (apply printf stuff))))
+
+(define (cache-fresh url)
+  ;; (debug "(cache-fresh ~S)~%" url)
+  ;; (debug "(current-time): ~S~%" (current-time))
+  ;; (if (current-time)
+  ;; 	  (debug "(current-time): ~S~%" (time->seconds (current-time))))
+
+  (and
+   (cached-time url)
+   (<= (- (time->seconds (current-time))
+		  (time->seconds (cached-time url)))
+	   cache-staleness-time)))
 
 (define (scrape url)
-  (or (alist-ref url page-cache string=?)
-    (let ((fetched-copy
-		   (with-input-from-request url #f read-string)))
-	  (update-cache url fetched-copy (current-time))
-      fetched-copy)))
+  (if (cache-fresh url)
+	  (begin
+		(debug "scrape: returning cached page: ~S~%" (cached-page url))
+		(cached-page url))
+	  (begin
+		(debug "scrape: doing fetch~%")
+		(let ((fetched-copy
+			   (with-input-from-request url #f read-string)))
+		  (debug "scrape: fetched-copy: ~S~%" fetched-copy)
+		  (update-cache url fetched-copy (current-time))
+		  fetched-copy))))
 
 (define (clear-page-cache)
   (set! page-cache '()))
