3ebc8d5560302ce96f90038d7124d0772cd969c0
John Maxwell <jmax@jmaxhome.com>
2019-12-11 23:41:50 -0500

    A bit more test cleanup.

--- spec/cache-spec.scm
+++ spec/cache-spec.scm
@@ -2,56 +2,55 @@
 (load "src/scraper")
 (declare (uses scraper templates))
 
+(define expected-page-body (make-parameter "some fake body"))
+(define expected-cache-timeout (make-parameter 3600))
+
+
+(define (with-input-from-request-stub url junk reader) (expected-page-body))
+(define (current-time-stub) (seconds->time 10000))
+
 (describe "scraping a page"
   (define passed-url "http://something.invalid")
   (before #:each
-    (stub! with-input-from-request (lambda (url junk reader) "")))
+    (stub! with-input-from-request with-input-from-request-stub)
+    (stub! current-time current-time-stub)
+    (scrape passed-url))
 
   (after #:each
     (clear-page-cache)
     (clear-stubs!))
 
   (describe "which is not in the cache"
-	(before #:each
-      (stub! with-input-from-request (lambda (url junk reader)
-                                       "some fake body"))
-      (stub! current-time (lambda () (seconds->time 10000)))
-
-      (scrape passed-url))
-
     (it "saves the page body"
       (expect (cached-page passed-url)
-			  (be "some fake body")))
+              (be "some fake body")))
 
     (it "saves the fetch time"
       (expect (cached-time passed-url)
-			  (be (current-time))))
+              (be (current-time))))
 
     (it "does a HTTP request"
-	  (clear-page-cache)
-	  (expect (scrape passed-url)
-			  (call with-input-from-request
-					(with passed-url #f read-string)
-					once))))
+      (clear-page-cache)
+      (expect (scrape passed-url)
+              (call with-input-from-request
+                    (with passed-url #f read-string)
+                    once))))
 
   (describe "which is in the cache and not expired"
-    (before #:each
-      (scrape passed-url))
-
     (it "doesn't do a HTTP request"
-      (expect ((lambda () (scrape passed-url)))
-			  (call with-input-from-request never))))
+      (expect (scrape passed-url)
+              (call with-input-from-request never))))
 
   (describe "which is in the cache but expired"
-    (define expected-cache-timeout 3600)
-
     (before #:each
-      (update-cache "a-url"
-					"page body"
-					(seconds->time (- (time->seconds (current-time))
-									  (+ 1 expected-cache-timeout)))))
+      (define just-stale-time
+        (seconds->time (- (time->seconds (current-time))
+                          (+ 1 (expected-cache-timeout)))))
+
+      (update-cache "a-url" "page body" just-stale-time))
+
     (it "does a new HTTP request"
       (expect (scrape "a-url")
-			  (call with-input-from-request
-					(with "a-url" #f read-string)
-					once)))))
+              (call with-input-from-request
+                    (with "a-url" #f read-string)
+                    once)))))
