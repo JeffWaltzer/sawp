0ce342b8ca5003a221c3ef41afb7e86ef26e7d36
John Maxwell <jmax@jmaxhome.com>
2019-12-22 13:46:20 -0500

    Check-in for branch switch

--- spec/cache-spec.scm
+++ spec/cache-spec.scm
@@ -1,10 +1,38 @@
 (use missbehave-stubs http-client)
 
+(define some-url "http://something-invalid")
+
+(define (preload-into-cache url)
+  (stub! with-input-from-request (returns #t))
+  (scrape url)
+  (clear-stubs!))
+
+
 (describe "scraping a page which is not in the cache"
   (before #:each
 	(load "src/scraper.scm")
 	(stub! with-input-from-request (returns #t)))
 
   (it "calls with-input-from-request"
-    (expect (scrape "http://something.invalid")
+	(expect (scrape some-url)
 			(call with-input-from-request once))))
+
+(describe "scraping a page which is in the cache"
+  (before #:each
+	(load "src/scraper.scm")
+	(preload-into-cache some-url)
+	(stub! with-input-from-request (returns #t)))
+
+  (it "does not call with-input-from-request"
+	(expect (scrape some-url)
+			(call with-input-from-request never))))
+
+(describe "save time of caching"
+  (before #:each
+	(load "src/scraper.scm")
+	(stub! with-input-from-request (returns #t)))
+
+  (it "saves the time"
+	  (scrape "http://something.invalid")
+	  (expect (cached-time passed-url)
+			  (current-time))))
--- src/scraper.scm
+++ src/scraper.scm
@@ -1,4 +1,13 @@
 (declare (unit scraper))
 (use http-client)
+
+(define cache '())
+
 (define (scrape url)
-	(with-input-from-request url #f read-string))
+  (or (alist-ref url cache string=?)
+	  (let ((fetched-copy
+			 (with-input-from-request url #f read-string)))
+		(set! cache (alist-update url fetched-copy cache string=?))
+		fetched-copy)))
+
+(define (cached-time url) #f)
