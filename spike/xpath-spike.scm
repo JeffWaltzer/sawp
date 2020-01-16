11e0c5e052381b933f854117c96d9ca731f75851
John Maxwell <jmax@jmaxhome.com>
2020-01-15 21:21:51 -0500

    Pushed spike to actually parsing the JSON and pulling out a value.
    Hooray!

--- spike/xpath-spike.scm
+++ spike/xpath-spike.scm
@@ -1,12 +1,11 @@
 (use html-parser)
 (use irregex)
+(use json)
 (use txpath)
+(use vector-lib)
+
 (define fooferd
-  ;span data-reactid="39"
-  ;//*[contains(text(),'ABC')]
-  (txpath "//script[contains(text(), 'root.App.main')]")
-  ;(txpath "//span[@data-reactid='39']")
-)
+  (txpath "//script[contains(text(), 'root.App.main')]"))
 
 (define head-extractor (irregex "root.App.main = ?(.*);"))
 (define tail-extractor (irregex ""))
@@ -14,7 +13,7 @@
 (define html
   (with-input-from-file "../examples/qcom-statistics.html"
     (lambda () (read-string))
-  )
+ )
 )
 (define parsed-html (html->sxml html))
 
@@ -22,5 +21,51 @@
 
 (define json-stuff (irregex-match-substring (irregex-search head-extractor js-stuff ) 1))
 
-(printf "~%~a~%"  json-stuff)
+(define parsed-json (call-with-input-string json-stuff json-read))
 
+(define (fix-json json)
+  (cond
+   ((list? json)
+	(map fix-json json))
+   ((vector? json)
+	(alist->hash-table
+	 (vector->list
+	  (vector-map
+	   (lambda (i kv-pair)
+		 `(,(car kv-pair) . ,(fix-json (cdr kv-pair))))
+	   json))))
+   (else
+	json)))
+
+(define (find-price-key path-so-far json)
+  (cond
+   ((hash-table? json)
+	(if (hash-table-exists? json "price")
+		(printf "~A~%" (reverse path-so-far))
+		(hash-table-map
+		 json
+		 (lambda (key value)
+		   (find-price-key (cons key path-so-far) value)))))
+   
+   ((list? json)
+	(for-each
+	 (lambda (value) (find-price-key (cons "[]" path-so-far) value))
+	 json)))
+
+  #f)
+
+(printf "~S~%"
+		(hash-table-ref
+		 (hash-table-ref
+		  (hash-table-ref
+		   (hash-table-ref
+			(hash-table-ref
+			 (hash-table-ref
+			  (hash-table-ref (fix-json parsed-json)
+							  "context")
+			  "dispatcher")
+			 "stores")
+			"QuoteSummaryStore")
+		   "price")
+		  "regularMarketOpen")
+		 "fmt"))
