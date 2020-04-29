479d92f700178ec33c05a70d4b07d5fe85472985
John Maxwell <jmax@jmaxhome.com>
2020-04-28 20:33:21 -0400

    Much improved specs for routing table.

--- spec/routing.spec.scm
+++ spec/routing.spec.scm
@@ -1,19 +1,36 @@
 (use missbehave-stubs)
 (use awful)
 
+(define (to-have-entry entry)
+  (matcher
+   (check (subject)
+          (let ((expected-value (force entry))
+                (routing-table (force subject)))
+            (any (lambda (routing-table-entry) (equal? expected-value routing-table-entry))
+                 routing-table)))
+   (message (form subject negate)
+            (if negate
+                (sprintf "Expected ~A not to have entry ~A" (force subject) entry)
+                (sprintf "Expected ~A to have entry ~A" (force subject) entry)))))
+
 (describe "the routing table"
+  (define routing-table '())
+
   (before #:each
-    (create-pages)
-    )
+    (stub! define-page (lambda (path handler #!key method)
+                         (for-each
+                          (lambda (a-method)
+                            (set! routing-table
+                              (cons `(,path ,handler ,a-method)
+                                    routing-table)))
+                          method)))
+    (create-pages))
 
-  (define (the-routing-table)
-    (hash-table->alist (awful-resources-table)))
+  (it "has an entry for GET-ing the main page"
+    (expect routing-table (to-have-entry `("/"  ,main-page-get GET))))
 
-  (it "something"
-    (expect (the-routing-table)
-            (be
-             `((("/" "/home/jmax/projects/sawp" GET)  ,main-page-get  . #f)
-               (("/" "/home/jmax/projects/sawp" HEAD) ,main-page-get  . #f)
-               (("/" "/home/jmax/projects/sawp" POST) ,main-page-post . #f))))
-    )
-  )
+  (it "has an entry for HEAD-ing the main page"
+    (expect routing-table (to-have-entry `("/"  ,main-page-get  HEAD))))
+
+  (it "has an entry for POST-ing the main page"
+    (expect routing-table (to-have-entry `("/"  ,main-page-post POST)))))
